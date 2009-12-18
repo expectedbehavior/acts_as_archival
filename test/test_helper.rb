9c3589b6d286064f73c48e6ac5de01a40324c136
Matt Gordon <matt@expectedbehavior.com>
2009-12-18 16:40:12 -0500

    moved the testing requirements around so everything needed for testing is self-contained to the plugin

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,17 @@
+$:.unshift(File.dirname(__FILE__) + '/../lib')
+RAILS_ROOT = File.dirname(__FILE__)
+
+require 'rubygems'
+require 'active_record'
+require "#{File.dirname(__FILE__)}/../lib/expected_behavior/acts_as_archival.rb"
+require "#{File.dirname(__FILE__)}/../lib/expected_behavior/acts_as_archival_active_record_methods.rb"
+require "#{File.dirname(__FILE__)}/../init"
+
+
 ENV["RAILS_ENV"] = "test"
-["/../../../../config/environment", 
+# "/aaa_test_app/config/environment", 
+# "/../../../../config/environment",
+["/aaa_test_app/config/environment", 
 #  "/../lib/expected_behavior/acts_as_archival_active_record_methods",
 #  "/../lib/expected_behavior/acts_as_archival",
  "/more_assertions",
