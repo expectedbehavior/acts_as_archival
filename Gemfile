17802c9ac13669b92b851649b8ebaa67ee6dfc1b
Joel Meador <joel@expectedbehavior.com>
2012-02-28 13:54:36 -0500

    cleanup

--- Gemfile
+++ Gemfile
@@ -3,4 +3,4 @@ source :rubygems
 gem "assertions-eb"
 gem "mysql2", "0.2.18"
 gem "rails",  "3.0.9"
-gem "pry"
+gem "rake", "0.8.7"
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,22 +1,15 @@
 $:.unshift(File.dirname(__FILE__) + '/../lib')
-#RAILS_ROOT = File.dirname(__FILE__)
 
 require "rubygems"
 require "active_record"
-require "#{File.dirname(__FILE__)}/../lib/expected_behavior/acts_as_archival.rb"
-require "#{File.dirname(__FILE__)}/../lib/expected_behavior/acts_as_archival_active_record_methods.rb"
-require "#{File.dirname(__FILE__)}/../init"
+require "expected_behavior/acts_as_archival.rb"
+require "expected_behavior/acts_as_archival_active_record_methods.rb"
+require_relative "../init"
 
 
 ENV["RAILS_ENV"] = "test"
-# "/aaa_test_app/config/environment",
-# "/../../../../config/environment",
-["/aaa_test_app/config/environment",
-#  "/../lib/expected_behavior/acts_as_archival_active_record_methods",
-#  "/../lib/expected_behavior/acts_as_archival",
- "/local_test_helper"
-].each do |file_to_load|
-  require File.expand_path(File.dirname(__FILE__) + file_to_load)
+["aaa_test_app/config/environment", "local_test_helper"].each do |file_to_load|
+  require_relative file_to_load
 end
 
 require 'rails/test_help'
