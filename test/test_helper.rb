4dc9560e5a0bdff16344e78921a6306ad0918748
Joel Meador <joel@expectedbehavior.com>
2012-02-28 13:26:42 -0500

    forgot to take the more_assertions file out

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,20 +1,19 @@
 $:.unshift(File.dirname(__FILE__) + '/../lib')
 #RAILS_ROOT = File.dirname(__FILE__)
 
-require 'rubygems'
-require 'active_record'
+require "rubygems"
+require "active_record"
 require "#{File.dirname(__FILE__)}/../lib/expected_behavior/acts_as_archival.rb"
 require "#{File.dirname(__FILE__)}/../lib/expected_behavior/acts_as_archival_active_record_methods.rb"
 require "#{File.dirname(__FILE__)}/../init"
 
 
 ENV["RAILS_ENV"] = "test"
-# "/aaa_test_app/config/environment", 
+# "/aaa_test_app/config/environment",
 # "/../../../../config/environment",
-["/aaa_test_app/config/environment", 
+["/aaa_test_app/config/environment",
 #  "/../lib/expected_behavior/acts_as_archival_active_record_methods",
 #  "/../lib/expected_behavior/acts_as_archival",
- "/more_assertions",
  "/local_test_helper"
 ].each do |file_to_load|
   require File.expand_path(File.dirname(__FILE__) + file_to_load)
