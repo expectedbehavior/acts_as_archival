dc46e24357a74b805058be120857a5a456749679
Joel Meador <joel@expectedbehavior.com>
2012-03-03 03:54:55 -0500

    remove the test rails app

--- test/aaa_test_app/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-AaaTestApp::Application.config.secret_token = 'efca13515e18956d38b7a7be2112c4b60139273679daa7e389ac198cc1cc6f8e9478edf48c5f43b75dd71a79aa98001bf14e6439e866b661a8407568027eba8c'
