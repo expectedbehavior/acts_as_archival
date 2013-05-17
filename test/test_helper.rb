a0975ee9d1e954d41d64617ed194b73ac1fcc1ee
Joel Meador <joel@expectedbehavior.com>
2013-05-17 13:01:00 -0400

    only test mass attribute stuff in active record if we are in rails 3 land; to be removed once rails 4 is standard

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,11 @@
 $:.unshift(File.dirname(__FILE__) + '/../lib')
 require "bundler/setup"
 require "test/unit"
+
 require "active_record"
 require "assertions"
 require "database_cleaner"
+
 require "acts_as_archival"
 
 def prepare_for_tests
@@ -43,19 +45,21 @@ def require_test_classes
     inflect.irregular "poly", "polys"
   end
 
-  [:archival,
+  fixtures = [:archival,
    :archival_kid,
    :archival_grandkid,
    :archival_table_name,
    :exploder,
    :independent_archival,
-   :mass_attribute_protected,
+   :plain,
    :missing_archived_at,
    :missing_archive_number,
    :plain,
    :poly,
-   :readonly_when_archived
-  ].each {|test_class_file| require_relative "fixtures/#{test_class_file}"}
+   :readonly_when_archived]
+  $require_mass_protection = ActiveModel.constants.include?(:MassAssignmentSecurity)
+  fixtures << :mass_attribute_protected if $require_mass_protection
+  fixtures.each {|test_class_file| require_relative "fixtures/#{test_class_file}"}
 end
 
 prepare_for_tests
