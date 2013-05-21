9886b29431845a54eb6fbbfc3922787aa4ee3487
Joel Meador <joel@expectedbehavior.com>
2013-05-21 01:03:37 -0400

    fix commentary on ambiguous table test

--- test/ambiguous_table_test.rb
+++ test/ambiguous_table_test.rb
@@ -1,12 +1,14 @@
 require_relative "test_helper"
 
 class AmbiguousTableTest < ActiveSupport::TestCase
-  # test against the problem fixed in http://github.com/DarkTatka/acts_as_archival/commit/63d0a2532a15d7a6ab41d081e1591108a5ea9b37
   test "no ambiguous table problem" do
     archival = Archival.create!
     child = archival.archivals.create!
     child.archive
 
+    # this is a bug fix for a problem wherein table names weren't being
+    # namespaced, so if a table joined against itself, incorrect SQL was
+    # generated
     assert_equal 1, Archival.unarchived.joins(:archivals).count
   end
 end
