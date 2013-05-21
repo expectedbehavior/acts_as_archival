21b2147178adbb9224e96e3de831effb3c9db769
Joel Meador <joel@expectedbehavior.com>
2013-05-21 10:08:47 -0400

    add some more assertions for deep nesting test so we know archive numbers are propagating

--- test/deep_nesting_test.rb
+++ test/deep_nesting_test.rb
@@ -2,14 +2,15 @@ require_relative "test_helper"
 
 class DeepNestingTest < ActiveSupport::TestCase
   test "archiving deeply nested items" do
-    archival = Archival.create!
-    child = archival.archivals.create!
+    archival   = Archival.create!
+    child      = archival.archivals.create!
     grandchild = child.archivals.create!
     archival.archive
-
     assert archival.reload.archived?
     assert child.reload.archived?
     assert grandchild.reload.archived?
+    assert_equal archival.archive_number, child.archive_number
+    assert_equal archival.archive_number, grandchild.archive_number
   end
 
   test "unarchiving deeply nested items doesn't blow up" do
@@ -17,11 +18,10 @@ class DeepNestingTest < ActiveSupport::TestCase
       :archived_at => Time.now,
       :archive_number => "test"
     }
-    archival = Archival.create!(archival_attributes)
-    child = archival.archivals.create!(archival_attributes)
+    archival   = Archival.create!(archival_attributes)
+    child      = archival.archivals.create!(archival_attributes)
     grandchild = child.archivals.create!(archival_attributes)
     archival.unarchive
-
     assert_not archival.reload.archived?
     assert_not child.reload.archived?
     assert_not grandchild.reload.archived?
