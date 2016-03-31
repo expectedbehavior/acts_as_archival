5f55824d1ed35f14b2e47818641b20724b478108
Joel Meador <joel@expectedbehavior.com>
2016-03-31 19:00:30 -0400

    use active record where.not instead of hand-building SQL

--- test/scope_test.rb
+++ test/scope_test.rb
@@ -49,7 +49,7 @@ class ScopeTest < ActiveSupport::TestCase
   end
 
   test "table_name is set to 'legacy'" do
-    archived_sql = %Q{SELECT "legacy".* FROM "legacy"  WHERE (legacy.archived_at IS NOT NULL AND legacy.archive_number IS NOT NULL)}
+    archived_sql = %Q{SELECT "legacy".* FROM "legacy"#  WHERE ("legacy"."archived_at" IS NOT NULL) AND ("legacy"."archive_number" IS NOT NULL)}
     unarchived_sql = %Q{SELECT "legacy".* FROM "legacy"  WHERE "legacy"."archived_at" IS NULL AND "legacy"."archive_number" IS NULL}
     assert_equal archived_sql, ArchivalTableName.archived.to_sql
     assert_equal unarchived_sql, ArchivalTableName.unarchived.to_sql
