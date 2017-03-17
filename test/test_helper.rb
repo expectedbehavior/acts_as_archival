6a355a870b0300005c3328f08a5e25e3f87b79e8
Joel Meador <joel@expectedbehavior.com>
2017-03-17 12:37:52 -0400

    we don't really need to use yaml to load a database configuration if there is only one database being tested

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,8 +13,7 @@ if ActiveSupport::TestCase.respond_to?(:test_order=)
 end
 
 def prepare_for_tests
-  setup_logging# if ENV["LOGGING_ENABLED"]
-  setup_active_record
+  setup_logging
   setup_database_cleaner
   create_test_tables
   require_test_classes
@@ -26,11 +25,6 @@ def setup_logging
   ActiveRecord::Base.logger = Logger.new(logfile)
 end
 
-def setup_active_record
-  dbconfig_file = File.dirname(__FILE__) + "/database.yml"
-  $dbconfig = YAML.load(File.read(dbconfig_file))
-end
-
 def setup_database_cleaner
   DatabaseCleaner.strategy = :truncation
   ActiveSupport::TestCase.send(:setup) do
@@ -38,10 +32,19 @@ def setup_database_cleaner
   end
 end
 
+def sqlite_config
+  {
+    adapter: "sqlite3",
+    database: "aaa_test.sqlite3",
+    pool: 5,
+    timeout: 5000
+  }
+end
+
 def create_test_tables
   schema_file = File.dirname(__FILE__) + "/schema.rb"
   puts "** Loading schema for SQLite"
-  ActiveRecord::Base.establish_connection($dbconfig['sqlite'])
+  ActiveRecord::Base.establish_connection(sqlite_config)
   load(schema_file) if File.exist?(schema_file)
 end
 
