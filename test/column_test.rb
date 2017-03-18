422220dc9dc295d27301e336898a302e3e66b9d5
Joel Meador <joel@expectedbehavior.com>
2017-03-18 11:59:54 -0400

    remove rubocop todo from multiline blocks defined inside {}

--- test/column_test.rb
+++ test/column_test.rb
@@ -2,14 +2,14 @@ require_relative "test_helper"
 
 class ColumnTest < ActiveSupport::TestCase
   test "acts_as_archival raises during create if missing archived_at column" do
-    assert_raises(ExpectedBehavior::ActsAsArchival::MissingArchivalColumnError) {
+    assert_raises(ExpectedBehavior::ActsAsArchival::MissingArchivalColumnError) do
       MissingArchivedAt.create!(name: "foo-foo")
-    }
+    end
   end
 
   test "acts_as_archival raises during create if missing archive_number column" do
-    assert_raises(ExpectedBehavior::ActsAsArchival::MissingArchivalColumnError) {
+    assert_raises(ExpectedBehavior::ActsAsArchival::MissingArchivalColumnError) do
       MissingArchiveNumber.create!(name: "rover")
-    }
+    end
   end
 end
